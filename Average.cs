5c1d0eb50def5bf590d4df241511e7ae16c153f5
Monsterter <ibrahimsenc@gmail.com>
2023-10-28 15:49:19 +0300

    Add project files.

---
+++ Average.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrtalamaHesabı
+{
+    public class Average
+    {
+        public double CAverage(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return (double)sum / arr.Length;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using OrtalamaHesabı;
+
+public class Program //Beklenilen sonuçları vermektedir
+{
+    static void Main(string[] args)
+    {
+        Fibonacci fibonacci = new Fibonacci();
+        Average average = new Average();
+
+        Console.Write("Derinlik giriniz:");
+        int depth = int.Parse(Console.ReadLine());
+
+        int[] series = fibonacci.CFibonacci(depth); // Fibonacci metodunda return edilen diziyi series int dizisine atadım	
+        double series_average = average.CAverage(series);
+        Console.Write("Fibonacci serisinin ortalaması: " + series_average);
+
+        Console.ReadKey();
+    }
+}
